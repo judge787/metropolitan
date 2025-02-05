0309184b507ab5aac163f102ad976984b2087c5c
WolfCrag <koutahebert@gmail.com>
2025-02-04 20:18:37 -0500

    website now displays product pitch; installed tailwindcss, which broke the formatting lol

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import ProductPitch from './components/ProductPitch';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-lg'>joblen</h1>
+      <ProductPitch />
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/ProductPitch.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ProductPitch: React.FC = () => {
+//   const olStyle = {
+//     textAlign: 'center' as const,
+//     listStylePosition: 'inside' as const,
+//   };
+
+  return (
+    <div>
+    <h2 className='text-2xl'>Product Pitch:</h2>
+    <p>Here's our product pitch:</p>
+    <p>What if you could easily track how housing and employment growth are shaping the future of Hamilton and Toronto—all in one place? Regional planners, real estate investors, and policymakers often struggle with fragmented data, making informed decisions difficult. The Metropolitan Housing and Employment Growth Index simplifies this by integrating housing statistics and employment trends into an intuitive platform with Line Charts and Radar Charts, enabling quick comparisons and insights. Imagine a city planner aligning development plans with real-time data—saving time and ensuring balanced growth. By transforming complex data into clear visuals, our tool boosts productivity, supports data-driven decisions, and maximizes ROI.</p>
+    </div>
+  );
+};
+
+export default ProductPitch;
