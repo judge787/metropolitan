05a787f3e07ff1230bf9bc10783053151b91e9ef
judge787 <jashanjudge87@gmail.com>
2025-03-04 18:47:39 -0500

    removed old cmmented code

--- ingester/src/DatabaseHandler.py
+++ ingester/src/DatabaseHandler.py
@@ -74,50 +74,6 @@ class DatabaseHandler:
         finally:
             cursor.close()
 
-    # def insert_housing_data(self, housing_data):
-    #     """
-    #     insert_housing_data: Insert a new housing data if it doesn't exist.
-    #     """
-    #     cursor = self.conn.cursor()
-    #     try:
-    #         # Check if housing data exists
-            
-    #         if cursor.fetchone() is None:
-    #             cursor.execute(
-    #                 """SELECT id FROM housing_data 
-    #                 WHERE census_metropolitan_area = ? 
-    #                 AND month = ? 
-    #                 AND total_starts = ? 
-    #                 AND total_complete = ? 
-    #                 AND singles_starts = ? 
-    #                 AND semis_starts = ? 
-    #                 AND row_starts = ? 
-    #                 AND apartment_starts = ? 
-    #                 AND singles_complete = ? 
-    #                 AND semis_complete = ? 
-    #                 AND row_complete = ? 
-    #                 AND apartment_complete = ?""",
-    #                 (
-    #                     housing_data.census_metropolitan_area,
-    #                     housing_data.month,
-    #                     housing_data.total_starts,
-    #                     housing_data.total_complete,
-    #                     housing_data.singles_starts,
-    #                     housing_data.semis_starts,
-    #                     housing_data.row_starts,
-    #                     housing_data.apartment_starts,
-    #                     housing_data.singles_complete,
-    #                     housing_data.semis_complete,
-    #                     housing_data.row_complete,
-    #                     housing_data.apartment_complete
-    #                 ),
-    #             )
-    #             self.conn.commit()
-    #     except mariadb.Error as e:
-    #         print(f"Error inserting housing data: {e}")
-    #     finally:
-    #         cursor.close()
-
     def insert_housing_data(self, housing_data):
         """
         insert_housing_data: Insert a new housing data if it doesn't exist.
