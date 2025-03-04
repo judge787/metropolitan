25f91b583865b1a9d42cce6f71e64d4896115e17
Chibuzor Okwusiuno <cokwusiu@uoguelph.ca>
2025-03-04 14:43:33 -0500

    updated backend with additional columns and updated tests

--- backend/src/main/java/com/metropolitan/backend/housing/models/Data.java
+++ backend/src/main/java/com/metropolitan/backend/housing/models/Data.java
@@ -21,16 +21,48 @@ public class Data {
     @Column(name = "month")
     private Integer month;
 
+    @Column(name = "singles_starts")
+    private Integer singleStarts;
+
+    @Column(name = "semis_starts")
+    private Integer semisStarts;
+
+    @Column(name = "row_starts")
+    private Integer rowStarts;
+
+    @Column(name = "apartment_starts")
+    private Integer apartmentStarts;
+
+    @Column(name = "singles_complete")
+    private Integer singlesComplete;
+
+    @Column(name = "semis_complete")
+    private Integer semisComplete;
+
+    @Column(name = "row_complete")
+    private Integer rowComplete;
+
+    @Column(name = "apartment_complete")
+    private Integer apartmentComplete;
+
     public Data() {
         
     }
-    
-    public Data(Integer id, String censusArea, Integer totalStarts, Integer totalComplete, Integer month) {
+
+    public Data(Integer id, String censusArea, Integer totalStarts, Integer totalComplete, Integer month, Integer singleStarts, Integer semisStarts, Integer rowStarts, Integer apartmentStarts, Integer singlesComplete, Integer semisComplete, Integer rowComplete, Integer apartmentComplete) {
         this.id = id;
         this.censusArea = censusArea;
         this.totalStarts = totalStarts;
         this.totalComplete = totalComplete;
         this.month = month;
+        this.singleStarts = singleStarts;
+        this.semisStarts = semisStarts;
+        this.rowStarts = rowStarts;
+        this.apartmentStarts = apartmentStarts;
+        this.singlesComplete = singlesComplete;
+        this.semisComplete = semisComplete;
+        this.rowComplete = rowComplete;
+        this.apartmentComplete = apartmentComplete;
     }
 
     public Integer getId() {
@@ -71,5 +103,69 @@ public class Data {
 
     public void setMonth(Integer month) {
         this.month = month;
+    }
+
+    public Integer getSingleStarts() {
+        return singleStarts;
+    }
+
+    public void setSingleStarts(Integer singleStarts) {
+        this.singleStarts = singleStarts;
+    }
+
+    public Integer getRowStarts() {
+        return rowStarts;
+    }
+
+    public void setRowStarts(Integer rowStarts) {
+        this.rowStarts = rowStarts;
+    }
+
+    public Integer getApartmentStarts() {
+        return apartmentStarts;
+    }
+
+    public void setApartmentStarts(Integer apartmentStarts) {
+        this.apartmentStarts = apartmentStarts;
+    }
+
+    public Integer getSemisStarts() {
+        return semisStarts;
+    }
+
+    public void setSemisStarts(Integer semisStarts) {
+        this.semisStarts = semisStarts;
+    }
+
+    public Integer getSinglesComplete() {
+        return singlesComplete;
+    }
+
+    public void setSinglesComplete(Integer singlesComplete) {
+        this.singlesComplete = singlesComplete;
+    }
+
+    public Integer getSemisComplete() {
+        return semisComplete;
+    }
+
+    public void setSemisComplete(Integer semisComplete) {
+        this.semisComplete = semisComplete;
+    }
+
+    public Integer getRowComplete() {
+        return rowComplete;
+    }
+
+    public void setRowComplete(Integer rowComplete) {
+        this.rowComplete = rowComplete;
+    }
+
+    public Integer getApartmentComplete() {
+        return apartmentComplete;
+    }
+
+    public void setApartmentComplete(Integer apartmentComplete) {
+        this.apartmentComplete = apartmentComplete;
     }
 }
